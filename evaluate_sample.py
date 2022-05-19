2a1b744a8c40af005ccffb3b441eab7a69ac2c31
Andrew Awad <andrew_usa90@hotmail.com>
2022-05-19 15:44:06 +0200

    Upadte: evaluate_sample.py to short sample videos to fit our model

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -20,15 +20,28 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
-
 import i3d
+
+import random
+import re
+import os
+import tempfile
+import cv2
+import argparse
+
+
+import imageio
+#import pyglet
+from IPython import display
+from moviepy.editor import *
+# from urllib import request 
+
+from preprocessing import load_video
 
 _IMAGE_SIZE = 224
 
-_SAMPLE_VIDEO_FRAMES = 79
 _SAMPLE_PATHS = {
-    'rgb': 'data/v_CricketShot_g04_c01_rgb.npy',
-    'flow': 'data/v_CricketShot_g04_c01_flow.npy',
+    'rgb': 'data/VID_NaderRun_rgb.npy',
 }
 
 _CHECKPOINT_PATHS = {
@@ -40,37 +53,53 @@ _CHECKPOINT_PATHS = {
 }
 
 _LABEL_MAP_PATH = 'data/label_map.txt'
-_LABEL_MAP_PATH_600 = 'data/label_map_600.txt'
 
 FLAGS = tf.flags.FLAGS
 
-tf.flags.DEFINE_string('eval_type', 'joint', 'rgb, rgb600, flow, or joint')
+tf.flags.DEFINE_string('eval_type', 'joint', 'rgb, rgb600, flow, or joint',)
 tf.flags.DEFINE_boolean('imagenet_pretrained', True, '')
 
+tf.flags.DEFINE_string('video_path', 'data/VID_NaderRun.mp4', 'path to video',)
 
-def main(unused_argv):
+def main(args):
+  
+
+  if not os.path.exists(args.video_path):
+        print("This file does not exist")
+        return
+  
+  # sample all video from video_path at specified frame rate (FRAME_RATE param)
+  sample_video = load_video(args.video_path,1000000000)	
+  MyVideo_Path = args.video_path
+  Video_duration=0
+  # sample_video = np.expand_dims(sample_video, axis=0)
+
+  video_name = args.video_path.split("/")[-1][:-4]
+  npy_rgb_output = 'data/' + video_name + '_rgb.npy'
+  np.save(npy_rgb_output, sample_video)
+
+
+  _SAMPLE_PATHS = {
+    'rgb': npy_rgb_output,
+  }
+
   tf.logging.set_verbosity(tf.logging.INFO)
   eval_type = FLAGS.eval_type
 
   imagenet_pretrained = FLAGS.imagenet_pretrained
 
   NUM_CLASSES = 400
-  if eval_type == 'rgb600':
-    NUM_CLASSES = 600
 
   if eval_type not in ['rgb', 'rgb600', 'flow', 'joint']:
     raise ValueError('Bad `eval_type`, must be one of rgb, rgb600, flow, joint')
 
-  if eval_type == 'rgb600':
-    kinetics_classes = [x.strip() for x in open(_LABEL_MAP_PATH_600)]
-  else:
-    kinetics_classes = [x.strip() for x in open(_LABEL_MAP_PATH)]
+  kinetics_classes = [x.strip() for x in open(_LABEL_MAP_PATH)]
 
   if eval_type in ['rgb', 'rgb600', 'joint']:
     # RGB input has 3 channels.
     rgb_input = tf.placeholder(
         tf.float32,
-        shape=(1, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 3))
+        shape=(None, None, _IMAGE_SIZE, _IMAGE_SIZE, 3))
 
 
     with tf.variable_scope('RGB'):
@@ -91,30 +120,11 @@ def main(unused_argv):
 
     rgb_saver = tf.train.Saver(var_list=rgb_variable_map, reshape=True)
 
-  if eval_type in ['flow', 'joint']:
-    # Flow input has only 2 channels.
-    flow_input = tf.placeholder(
-        tf.float32,
-        shape=(1, _SAMPLE_VIDEO_FRAMES, _IMAGE_SIZE, _IMAGE_SIZE, 2))
-    with tf.variable_scope('Flow'):
-      flow_model = i3d.InceptionI3d(
-          NUM_CLASSES, spatial_squeeze=True, final_endpoint='Logits')
-      flow_logits, _ = flow_model(
-          flow_input, is_training=False, dropout_keep_prob=1.0)
-    flow_variable_map = {}
-    for variable in tf.global_variables():
-      if variable.name.split('/')[0] == 'Flow':
-        flow_variable_map[variable.name.replace(':0', '')] = variable
-    flow_saver = tf.train.Saver(var_list=flow_variable_map, reshape=True)
-
-  if eval_type == 'rgb' or eval_type == 'rgb600':
-    model_logits = rgb_logits
-  elif eval_type == 'flow':
-    model_logits = flow_logits
-  else:
-    model_logits = rgb_logits + flow_logits
+ 
+  model_logits = rgb_logits 
   model_predictions = tf.nn.softmax(model_logits)
-
+  
+  #restore the model weights from the checkpoints
   with tf.Session() as sess:
     feed_dict = {}
     if eval_type in ['rgb', 'rgb600', 'joint']:
@@ -125,31 +135,98 @@ def main(unused_argv):
       tf.logging.info('RGB checkpoint restored')
       rgb_sample = np.load(_SAMPLE_PATHS['rgb'])
       tf.logging.info('RGB data loaded, shape=%s', str(rgb_sample.shape))
-      feed_dict[rgb_input] = rgb_sample
+      
+      
+      #Convert long video to a list of short sample videos 
+      #MyVideo_Path ="data/cricket.avi"  # Video path 
+      sample_frame = 80
+      Output_Videos =[]
+      sample_list = []
 
-    if eval_type in ['flow', 'joint']:
-      if imagenet_pretrained:
-        flow_saver.restore(sess, _CHECKPOINT_PATHS['flow_imagenet'])
-      else:
-        flow_saver.restore(sess, _CHECKPOINT_PATHS['flow'])
-      tf.logging.info('Flow checkpoint restored')
-      flow_sample = np.load(_SAMPLE_PATHS['flow'])
-      tf.logging.info('Flow data loaded, shape=%s', str(flow_sample.shape))
-      feed_dict[flow_input] = flow_sample
 
-    out_logits, out_predictions = sess.run(
-        [model_logits, model_predictions],
+      for x in range(int(rgb_sample.shape[0]/sample_frame)):
+        sample_list.append(rgb_sample[x*sample_frame:(x+1)*sample_frame])
+
+      if len(sample_list) == 0 :
+        sample_list.append(rgb_sample)
+
+      # Run the i3d model on the list of videos and print the top 5 actions of every video.
+      # First add an empty dimension to the sample video as the model takes as input
+      # a batch of videos.
+
+      
+	    My_Counter=0
+      My_clip =VideoFileClip(MyVideo_Path)
+      Video_duration = np.round(My_clip.duration,1) ##### by seconds 
+      Max_video_duration = np.round(Video_duration/len(sample_list),1)
+      My_start=0 
+      My_End=Max_video_duration
+
+      ######  intial values for cutting the long video to short videos 
+
+      
+      for x in sample_list:
+        model_input = np.expand_dims(x, axis=0)
+        feed_dict[rgb_input] = model_input
+
+        out_logits, out_predictions = sess.run([model_logits, model_predictions],
         feed_dict=feed_dict)
 
-    out_logits = out_logits[0]
-    out_predictions = out_predictions[0]
-    sorted_indices = np.argsort(out_predictions)[::-1]
+        out_logits = out_logits[0]
+        out_predictions = out_predictions[0]
+        sorted_indices = np.argsort(out_predictions)[::-1]
 
-    print('Norm of logits: %f' % np.linalg.norm(out_logits))
-    print('\nTop classes and probabilities')
-    for index in sorted_indices[:20]:
-      print(out_predictions[index], out_logits[index], kinetics_classes[index])
+          #print('Norm of logits: %f' % np.linalg.norm(out_logits))
+          #print('\nTop 5 classes and probabilities')
+        My_count=0
+        for index in sorted_indices[:5]:
+            #print("%-22s %.2f%%" % (kinetics_classes[index], out_predictions[index] * 100))
+            if My_count ==0 :
+              result_text = kinetics_classes[index]
+              My_count = My_count+1
+			        break
+        
+        ###### Adding the text of the video action at the bottom .. 
+
+        clip = VideoFileClip(MyVideo_Path).subclip(My_start,My_End)
+        txt_clip = ( TextClip(result_text,fontsize=50,color='white')
+              .set_position('bottom')
+              .set_duration(Max_video_duration)
+                )
+        if My_start!=0 and My_start>=Video_duration:
+           break 
+        if My_start!=0 and My_End>=Video_duration:
+            My_End = Video_duration
+        video = CompositeVideoClip([clip, txt_clip])
+        OutPut_name = str(My_Counter)+".mp4"
+        video.write_videofile(OutPut_name,fps=25)
+        Output_Videos.append(VideoFileClip(OutPut_name))
+        clip.close()
+        My_Counter=My_Counter+1
+        My_start = My_start +Max_video_duration
+        My_End = My_End +Max_video_duration
+
+ 
+# concatenate all the results videos 
+      Result_Video = concatenate_videoclips(Output_Videos ,method='compose')
+      Result_Video.write_videofile("output_videos/Multi_10.mp4",fps=25) 
+  
+
+
+
+
+
+          
+ 
 
 
 if __name__ == '__main__':
-  tf.app.run(main)
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--video_path', type=str, default="data/VID_NaderRun.mp4")
+
+  args = parser.parse_args()
+  main(args)
+  #tf.compat.v1.app.run (main(args))
+
+  
